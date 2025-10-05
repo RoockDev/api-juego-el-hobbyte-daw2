6c7c402dcea9ac9b844abac2164dc419c550d50e
Sergio Zarcero Perez <zar0_31@outlook.com>
2025-10-05 18:26:26 +0200

    feat: Metodo create User controlador Usuario
    se realiza el metodo que permite crear usuarios solo disponible para administradores

--- Controller/usuarioController.php
+++ Controller/usuarioController.php
@@ -100,4 +100,79 @@ class UsuarioController
             echo json_encode(['error' => 'error al obtener el usuario '.$e->getMessage()]);
         }
     }
+
+    //Post /admin/users
+    //Crear un nuevo usuario (solo disponible para los administradores)
+    public function createUser(){
+        if ($this->usuarioAutenticado === null) {
+            http_response_code(401);
+            echo json_encode(['error' => 'Usuario no autenticado']);
+            return;
+        }
+
+        if ($this->usuarioAutenticado->getRolNombre() !== 'admin') {
+            http_response_code(403);
+            echo json_encode(['error' => 'Acceso Denegado, solo los administradores pueden ver esta información']);
+            return;
+        }
+
+        $datos = json_decode(file_get_contents('php://input'), true);
+
+        $camposObligatorios = ['dni', 'clave', 'email', 'nombre', 'rol_id'];
+    foreach ($camposObligatorios as $campo) {
+        if (!isset($datos[$campo]) || trim($datos[$campo]) === '') {
+            http_response_code(400);
+            echo json_encode(['error' => "El campo '$campo' es obligatorio y no puede estar vacío"]);
+            return;
+        }
+    }
+
+    $dni = trim($datos['dni']);
+    $clave = $datos['clave']; 
+    $email = trim($datos['email']);
+    $nombre = trim($datos['nombre']);
+    $rol_id = (int)$datos['rol_id'];
+
+    if ($rol_id !== 1 && $rol_id !== 2) {
+        http_response_code(400);
+        echo json_encode(['error' => 'El rol_id debe ser 1 (admin) o 2 (gamer)']);
+        return;
+    }
+
+    try {
+        if (UsuarioDAO::getUserByDni($dni) !== null) {
+            http_response_code(409);
+            echo json_encode(['error' => 'ese dni ya esta registrado']);
+            return;
+        }
+
+        if (usuarioDAO::getUserByEmail($email) !== null) {
+            http_response_code(409);
+            echo json_encode(['error' => 'ese email ya esta registrado']);
+            return;
+        }
+
+
+        $nuevoUsuario = new Usuario();
+        $nuevoUsuario->setDni($dni);
+        $nuevoUsuario->setClave($clave);
+        $nuevoUsuario->setEmail($email);
+        $nuevoUsuario->setNombre($nombre);
+        $nuevoUsuario->setRolId($rol_id);
+
+        $exito = usuarioDAO::createUser($nuevoUsuario);
+
+        echo json_encode([
+            'mensaje' => 'Usuario Creado Correctamente',
+            'id' => $nuevoUsuario->getId(),
+            'nombre' => $nuevoUsuario->getNombre(),
+            'email' => $nuevoUsuario->getEmail(),
+            'rol' =>$nuevoUsuario->getRolNombre()
+        ]);
+    } catch (Exception $e) {
+        http_response_code(500);
+        echo json_encode(['error' => 'error al crear usuario']);
+    }
+
+    }
 }
