79f080cc7c758f384e8a76b7a291650c5fabf959
Sergio Zarcero Perez <zar0_31@outlook.com>
2025-10-05 16:51:27 +0200

    feat: Metodo Obtener Usuarios controlador Usuario
    Realizamos el primer metodo en el controlador, el cual es obtener todos los usuarios, al cual solo el adminsitrador podra acceder

---
+++ Controller/usuarioController.php
@@ -0,0 +1,59 @@
+<?php
+
+require_once __DIR__ . '/../Models/Usuario.php';
+require_once __DIR__ . '/../DataBase/UsuarioDAO.php';
+
+class UsuarioController
+{
+
+    private $usuarioAutenticado;
+
+    public function __construct(Usuario $usuarioAutenticado)
+    {
+        $this->usuarioAutenticado = $usuarioAutenticado;
+    }
+
+    //Get /admin/users 
+    //obtener todos los usuarios (solo disponible para los administradores)
+    public function getAllUsers()
+    {
+       
+        
+        if ($this->usuarioAutenticado === null) {
+            http_response_code(401);
+            echo json_encode(['error' => 'Usuario no autenticado']);
+            return;
+        }
+
+        if ($this->usuarioAutenticado->getRolNombre() !== 'admin') {
+            http_response_code(403);
+            echo json_encode(['error' => 'Acceso Denegado, solo los administradores pueden ver esta información']);
+            return;
+        }
+
+        try {
+            $usuarios = UsuarioDAO::getAllUsers();
+            if ($usuarios === null) {
+                $usuarios = [];
+            }
+
+            $usuariosProtegidos = [];
+
+            foreach ($usuarios as $usuario) {
+                $usuariosProtegidos[] = [
+                    'id' => $usuario->getId(),
+                    'dni' => $usuario->getDni(),
+                    'email' => $usuario->getEmail(),
+                    'nombre' => $usuario->getNombre(),
+                    'rol' => $usuario->getRolNombre()
+                ];
+            }
+
+            http_response_code(200);
+            echo json_encode($usuariosProtegidos);
+        } catch (Exception $e) {
+            http_response_code(500);
+            echo json_encode(['error' => 'Error al obtener la lista de usuarios']);
+        }
+    }
+}
