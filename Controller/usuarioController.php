0091ad524a7ab1d982e66a70eb8745ae4150e724
Sergio Zarcero Perez <zar0_31@outlook.com>
2025-10-05 17:17:02 +0200

    feat: MEtodo obtener usuario por Id controlador Usuario
    se realiza el metodo para obtener un usuario por ID , con sus correspondientes validaciones de erroes y hecho sàra ser utilizado unicamente por el admin

--- Controller/usuarioController.php
+++ Controller/usuarioController.php
@@ -34,7 +34,9 @@ class UsuarioController
         try {
             $usuarios = UsuarioDAO::getAllUsers();
             if ($usuarios === null) {
-                $usuarios = [];
+                http_response_code(404);
+                echo json_encode(['error' => 'usuarios no encontrado']);
+                return;
             }
 
             $usuariosProtegidos = [];
@@ -54,6 +56,48 @@ class UsuarioController
         } catch (Exception $e) {
             http_response_code(500);
             echo json_encode(['error' => 'Error al obtener la lista de usuarios']);
+        }
+    }
+
+
+    //Get /admin/users/id
+    //Obtener un usuario por Id (solo disponible para los administradores)
+    
+    public function getUserById($id){
+        if ($this->usuarioAutenticado === null) {
+            http_response_code(401);
+            echo json_encode(['error' => 'Usuario no autenticado']);
+            return;
+        }
+
+        if ($this->usuarioAutenticado->getRolNombre() !== 'admin') {
+            http_response_code(403);
+            echo json_encode(['error' => 'Acceso Denegado, solo los administradores pueden ver esta información']);
+            return;
+        }
+
+        try {
+    
+            $usuario = UsuarioDAO::getUserById($id);
+
+            if ($usuario === null) {
+                http_response_code(404);
+                echo json_encode(['error' => 'usuario no encontrado' ]);
+                return;
+            }
+            $usuarioProtegido = [
+                'id' => $id,
+                'dni' => $usuario->getDni(),
+                'email' => $usuario->getEmail(),
+                'nombre' => $usuario->getNombre(),
+                'rol' => $usuario->getRolNombre()
+            ];
+            http_response_code(200);
+            echo json_encode($usuarioProtegido);
+
+        } catch (Exception $e) {
+            http_response_code(500);
+            echo json_encode(['error' => 'error al obtener el usuario '.$e->getMessage()]);
         }
     }
 }
